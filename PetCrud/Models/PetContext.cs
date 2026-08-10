a5383b70a57ddbc1d3895e4fc1cf15f03c9c4bc6
guilherme-ogreofcodes <guilhermeieoq@gmail.com>
2026-08-10 07:58:48 -0300

    Adicionar arquivos de projeto.

---
+++ PetCrud/Models/PetContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetCrud.Models;
+
+public class PetContext : DbContext
+{
+
+    public PetContext(DbContextOptions<PetContext> options)
+        : base(options)
+    {
+    }
+
+
+    public DbSet<Animal> Animals { get; set; }
+}
